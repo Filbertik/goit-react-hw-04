0059c3c56748723ad23ab287e93c098a8e63bc4b
Filbertik <aton2019zvit@gmail.com>
2025-04-06 00:49:08 +0300

    edits 1

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,29 +1,22 @@
 // import s from "./Loader.module.css";
-// import { DNA } from "react-loader-spinner";
+// import { HashLoader } from "react-spinners";
 
 // const Loader = () => {
 //   return (
 //     <div className={s.loader}>
-//       <DNA
-//         visible={true}
-//         height="100"
-//         width="100"
-//         ariaLabel="dna-loading"
-//         wrapperStyle={{}}
-//         wrapperClass="dna-wrapper"
-//       />
+//       <HashLoader color="#36d7b7" size={100} />
 //     </div>
 //   );
 // };
-// export default Loader;
 
+// export default Loader;
+import { ClipLoader } from "react-spinners";
 import s from "./Loader.module.css";
-import { HashLoader } from "react-spinners";
 
 const Loader = () => {
   return (
     <div className={s.loader}>
-      <HashLoader color="#36d7b7" size={100} />
+      <ClipLoader size={50} color="#36d7b7" />
     </div>
   );
 };
